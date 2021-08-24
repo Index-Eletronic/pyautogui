4e78c34d477c67d515463bd5f9e05a453e7efb7f
Fil Tuon <index.eletronic@gmail.com>
2021-08-24 13:59:18 -0300

    .

---
+++ Make_backup.py
@@ -0,0 +1,17 @@
+import pyautogui
+from time import sleep
+
+pyautogui.alert('NÃO MEXA NO COMPUTADOR.')
+pyautogui.PAUSE = 0.5
+#Abrir o Google Drive no Computador
+pyautogui.press('winleft')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+sleep(2)
+pyautogui.write('https://drive.google.com/drive/my-dive')
+pyautogui.press('enter')
+#Acessar a area de trabalho
+#Clicar no arquivo para back e arrastar ele para o Google Drive
+#Entrar no Google Drive
+#Deixar o arquivo.
+#Esperar 5 segundos.
